a1d0f6da5a1e51ff3e44ff0a95ae7b65daaa3763
Leeje408 <leeje408@gmail.com>
2016-01-08 15:32:58 +0800

    更改mo()函数，为接收4个参数，分别为元素，x/y方向对象（初始位置，宽/高，每帧偏移），时间

--- f.js
+++ f.js
@@ -2,6 +2,7 @@ window.onload = function(){
     moveA();
     moveB();
 };
+//创建单向数据（x/y）
 function createMo(show, bp, offset){
     this.show = show;
     this.po = 0;
@@ -13,14 +14,15 @@ function moveB() {
     var px = new createMo(show,6000,-75);
     mo(show, px, 10);
 }
-function mo() {
+//show:显示元素 px:横向信息（默认图片横向排布【必填】），包含po（初始位置）、bp（最大值）、offset（偏移量） py:与px相似，当仅为一行时，可省略 time:每帧时间
+function mo() {   
     var show, time;
     var px = new Object();
     var py = new Object();
     if(arguments.length == 3){
         show = arguments[0];
         px = arguments[1];
-        py = { poy:0, bpy:1, offset:0}
+        py = { po:0, bp:1, offset:0}
         time = arguments[2];
     } else if(arguments.length == 4){
         show = arguments[0];
@@ -32,11 +34,11 @@ function mo() {
         return false;
     }
     this.st = setTimeout(function (){
-        px.pox %= px.bpx;
-        py.poy %= py.bpy;
-        show.style.backgroundPosition = px.pox + "px " + py.poy + "px";
-        if(px.pox == 0) { py.poy += py.offset; }
-        px.pox += px.offset;
+        px.po %= px.bp;
+        py.po %= py.bp;
+        show.style.backgroundPosition = px.po + "px " + py.po + "px";
+        if(px.po == 0) { py.po += py.offset; }
+        px.po += px.offset;
         mo(show, px, py, time);
         }, time); 
 }
@@ -44,11 +46,11 @@ function moveA(){
     var show = document.getElementById("b");
     var px = new Object();
     var py = new Object();
-    px.pox = 560;
-    px.bpx = px.pox;
+    px.po = 560;
+    px.bp = px.po;
     px.offset = -140;
-    py.poy = 160;
-    py.bpy = py.poy;
+    py.po = 160;
+    py.bp = py.po;
     py.offset = 80;
     mo(show, px, py, 100);
 }
